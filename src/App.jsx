9d01beff8cfc48965daac3785f455a8fad4a918d
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-16 19:39:01 +1100

    Added Introduction

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { useState } from 'react'
 import { Navbar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
 
 function App() {
   const [isLoading, setIsLoading] = useState(false)
@@ -15,6 +16,7 @@ function App() {
     <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity-100' : 'opacity-0' } bg-black text-white-100`}>
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
 
     </div>
     </>
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+    return (
+        <section id="home" className="min-h-screen flex items-center justify-center relative">
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-gray-400 via-pink-500 to-rose-400 bg-clip-text text-transparent leading-tight ">Hi, I'm Claire Nguyen</h1>
+
+                <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                    I'm a Business Analytics undergraduate at Deakin University passionate about turning data into insights. I'm fascinated by the intersection of business strategy and data-driven decision making, with the goal of solving real-world business challenges through analytics.
+                </p>
+            </div>
+        </section>
+    )
+}
