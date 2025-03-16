bfceaed4465792a2f8d7a4622b63664bab958168
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-16 19:06:00 +1100

    Added moblie menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import { LoadingScreen } from './components/LoadingScreen'
 import './index.css'
 import { useState } from 'react'
 import { Navbar } from './components/Navbar'
+import { MobileMenu } from './components/MobileMenu'
 
 function App() {
   const [isLoading, setIsLoading] = useState(false)
+  const[menuOpen, setMenuOpen] = useState(false)
   return (
     <>
     {!isLoading && <LoadingScreen onComplete = {() => setIsLoading(true)} />}
     <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity-100' : 'opacity-0' } bg-black text-white-100`}>
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 
     </div>
     </>
