d6493bbde9768418510c0fff4b04fd7091e37d51
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-16 18:24:59 +1100

    Added navbar component with Website Name as logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import './index.css'
 import { useState } from 'react'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const [isLoading, setIsLoading] = useState(false)
   return (
     <>
     {!isLoading && <LoadingScreen onComplete = {() => setIsLoading(true)} />}
-    <div></div>
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity-100' : 'opacity-0' } bg-black text-white-100`}>
+      <Navbar />
+
+    </div>
     </>
   )
 }
