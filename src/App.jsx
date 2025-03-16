95bd5a4b57c512ebda5dd82b352fbc0a05ab4d09
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-16 17:59:09 +1100

    Added Loading text animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import './index.css'
+import { useState } from 'react'
 
 function App() {
-  return <></>
+  const [isLoading, setIsLoading] = useState(false)
+  return (
+    <>
+    {!isLoading && <LoadingScreen onComplete = {() => setIsLoading(true)} />}
+
+    </>
+  )
 }
 
 export default App
